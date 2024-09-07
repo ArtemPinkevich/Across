17f09c28b9c952a1552026440c0c8e9c20358c07
PinkevichAV <PinkevichAV@elcomplus.ru>
2024-09-07 22:07:20 +0700

    Миграция бд

---
+++ Backend/DataAccess.SqlLite/Migrations/20240907122903_TransportationStatusDeleted.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.SqlLite.Migrations
+{
+    public partial class TransportationStatusDeleted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TransportationStatus",
+                table: "TransportationStatusRecords");
+
+            migrationBuilder.DropColumn(
+                name: "TransportationStatus",
+                table: "Transportations");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TransportationStatus",
+                table: "TransportationStatusRecords",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TransportationStatus",
+                table: "Transportations",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
