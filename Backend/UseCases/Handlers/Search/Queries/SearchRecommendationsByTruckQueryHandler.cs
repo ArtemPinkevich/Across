4bf6d38c0d3491f96d27ac860fd6c57e43208609
PinkevichAV <PinkevichAV@elcomplus.ru>
2024-09-07 22:11:14 +0700

    Исключение из рекомендаций водителя тех что уже в заявке

--- Backend/UseCases/Handlers/Search/Queries/SearchRecommendationsByTruckQueryHandler.cs
+++ Backend/UseCases/Handlers/Search/Queries/SearchRecommendationsByTruckQueryHandler.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using DataAccess.Interfaces;
+using Entities;
 using MediatR;
 using UseCases.Handlers.Cargo.Dto;
 using UseCases.Handlers.Common.Dto;
@@ -15,14 +16,17 @@ public class SearchRecommendationsByTruckQueryHandler : IRequestHandler<SearchRe
     private readonly IMapper _mapper;
     private readonly IRepository<Entities.Truck> _trucksRepository;
     private readonly IRepository<Entities.TransportationOrder> _ordersRepository;
+    private readonly IRepository<DriverRequest> _driverRequestRepository;
 
     public SearchRecommendationsByTruckQueryHandler(IMapper mapper,
         IRepository<Entities.Truck> trucksRepository,
-        IRepository<Entities.TransportationOrder> ordersRepository)
+        IRepository<Entities.TransportationOrder> ordersRepository,
+        IRepository<DriverRequest> driverRequestRepository)
     {
         _mapper = mapper;
         _trucksRepository = trucksRepository;
         _ordersRepository = ordersRepository;
+        _driverRequestRepository = driverRequestRepository;
     }
     
     public async Task<SearchResultDto> Handle(SearchRecommendationsByTruckQuery request, CancellationToken cancellationToken)
@@ -44,10 +48,14 @@ public class SearchRecommendationsByTruckQueryHandler : IRequestHandler<SearchRe
                                                      && x.TruckRequirements.InnerBodyLength >= truck.InnerBodyLength
                                                      && x.TruckRequirements.InnerBodyWidth >= truck.InnerBodyWidth);
 
+        var driverRequests = await _driverRequestRepository.GetAllAsync(o => o.TruckId == request.TruckId);
+        var filtredOrders = orders.Where(o => !driverRequests.Any(r => r.TransportationOrderId == o.Id));
+
+
         return new SearchResultDto()
         {
             Result = ApiResult.Success,
-            TransportationOrders = orders.Select(x => _mapper.Map<TransportationOrderDto>(x)).ToList()
+            TransportationOrders = filtredOrders.Select(x => _mapper.Map<TransportationOrderDto>(x)).ToList()
         };
     }
 }
